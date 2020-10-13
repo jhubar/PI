f9f2afe8d2eaca66c2be072e99e726baa90ecd6c
Hubar Julien <Julien1941@live.be>
2020-10-13 21:41:58 +0200

    update linear link

---
+++ js/PythonChart/chart-area-ActiveCases-regLin.js
@@ -0,0 +1,314 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+loadData();
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+function loadData(){
+  var url = "https://raw.githubusercontent.com/julien1941/PI/master/Python/Data/data.json?token=AL3RLGI2OBSKG2VGVVPAJYS7R4UOA"
+  var data = ''
+  // DAp
+  var tmp ;
+  $.get(url,function(data){
+
+    const result = JSON.parse(data);
+
+
+
+
+
+    label = [];
+    label_pred = [];
+    dataL = [];
+    // dataOverfit = [];
+    // dataUnderfit = [];
+    dataLinearfit = [];
+    console.log(result.current)
+    console.log(result.prediction)
+    for(var i=0;i<result.current.length;i++){
+
+      dataL.push(result.current[i].num_positive);
+
+
+    }
+
+    
+
+    for(var i=0;i<result.prediction.length;i++){
+      label.push(result.prediction[i].Day);
+
+
+      // dataOverfit.push(result.prediction[i].num_positive);
+      // dataUnderfit.push(result.prediction[i].num_positive);
+      dataLinearfit.push(result.prediction[i].num_positive);
+    }
+
+
+
+  var ctx = document.getElementById("regLin-chart");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: label,
+      datasets: [
+        {
+          label: "Current ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(78, 115, 223, 0.2)",
+          borderColor: "rgba(78, 115, 223, 1)",
+          pointRadius: 4,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 4,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 4,
+          data: dataL,
+        },
+        // //Underfit line
+        // {
+        //   label: "Underfit ",
+        //   lineTension: 0.1,
+        //   backgroundColor: "rgba(255,255,255,1)",
+        //   borderColor: "rgba(255, 193, 7,0.1)",
+        //   pointRadius: 3,
+        //   pointBackgroundColor: "rgba(255, 193, 7,1)",
+        //   pointBorderColor: "rgba(255, 193, 7,1)",
+        //   pointHoverRadius: 3,
+        //   pointHoverBackgroundColor: "rgba(255, 193, 7, 1)",
+        //   pointHoverBorderColor: "rgba(255, 193, 7, 1)",
+        //   pointHitRadius: 10,
+        //   pointBorderWidth: 2,
+        //   data: dataUnderfit,
+        // },
+        // linear line
+        {
+          label: "Linear ",
+          lineTension: 0.3,
+          backgroundColor: "rgba(255, 193, 7,0.1)",
+          borderColor: "rgba(237, 0, 59, 1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointBorderColor: "rgba(237, 0, 59, 1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointHoverBorderColor: "rgba(237, 0, 59, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          data: dataLinearfit,
+        },
+        // //Overfitt line
+        // {
+        //   label: "Overfit ",
+        //   lineTension: 0.1,
+        //   backgroundColor: "rgba(255, 193, 7,0.3)",
+        //   borderColor: "rgba(255, 193, 7,0.1)",
+        //   pointRadius: 3,
+        //   pointBackgroundColor: "rgba(255, 193, 7,1)",
+        //   pointBorderColor: "rgba(255, 193, 7,1)",
+        //   pointHoverRadius: 3,
+        //   pointHoverBackgroundColor: "rgba(255, 193, 7, 1)",
+        //   pointHoverBorderColor: "rgba(255, 193, 7, 1)",
+        //   pointHitRadius: 10,
+        //   pointBorderWidth: 2,
+        //   data: dataOverfit,
+        // }
+
+
+
+
+    ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 7,
+            // callback: function(value, index, values) {
+            //   return number_format(value)+ ' Days';
+            // }
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            maxTicksLimit: 5,
+            padding: 10,
+
+            callback: function(value, index, values) {
+              return number_format(value)+ ' Cases';
+            }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + number_format(tooltipItem.yLabel)+ ': Cases';
+          }
+        }
+      }
+    }
+  },);
+},
+);
+
+}
+
+label = [];
+dataL = [];
+// Area Chart Example
+var ctx = document.getElementById("regLin-chart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: label,
+    datasets: [{
+      label: "Current ",
+      lineTension: 0.3,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: dataL,
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7,
+          // callback: function(value, index, values) {
+          //   return number_format(value)+ ' Days';
+          // }
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+
+          callback: function(value, index, values) {
+            return number_format(value)+ ' Cases';
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + number_format(tooltipItem.yLabel)+ ': Cases';
+        }
+      }
+    }
+  }
+});
+loadData();
