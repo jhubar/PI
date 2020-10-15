c90633e080b0c96a19a81ef0d1a779cc7531c2ae
Hubar Julien <Julien1941@live.be>
2020-10-15 15:02:03 +0200

    update load data

--- js/demo/chart-area-ActiveCases.js
+++ js/demo/chart-area-ActiveCases.js
@@ -139,7 +139,7 @@ function loadData(){
           pointHoverBorderColor: "rgba(34,139,34, 0.1)",
           pointHitRadius: 10,
           pointBorderWidth: 4,
-          data: cum_cases(0,dataC),
+          data: cum_cases(1,dataC),
         },
         // current cases
         {
@@ -285,99 +285,3 @@ function loadData(){
 );
 
 }
-
-label = [];
-dataL = [];
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: label,
-    datasets: [{
-      label: "Current ",
-      lineTension: 0.3,
-      backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(78, 115, 223, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: dataL,
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 7,
-          // callback: function(value, index, values) {
-          //   return number_format(value)+ ' Days';
-          // }
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-
-          callback: function(value, index, values) {
-            return number_format(value)+ ' Cases';
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + number_format(tooltipItem.yLabel)+ ': Cases';
-        }
-      }
-    }
-  }
-});
-loadData();
