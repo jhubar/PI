4396aae6505ab1947a930c1cebd83665eaf29a56
Hubar Julien <Julien1941@live.be>
2020-10-15 16:53:17 +0200

    update button

--- js/demo/chart-area-ActiveCases.js
+++ js/demo/chart-area-ActiveCases.js
@@ -2,6 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 loadData();
+
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
@@ -26,100 +27,10 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   }
   return s.join(dec);
 }
-function cum_cases(ans, dataC){
-  if(ans == 1){
-    return dataC;
-  }else{
-    return [];
-  }
+function without_cum_cases() {
+  // ctx.destroy();
+  ctx = document.getElementById("myAreaChart");
 
-}
-function loadData(){
-  var url = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv"
-  var data = ''
-  // DAp
-  var tmp ;
-  $.get(url,function(data){
-    var result = [];
-    var lines=data.split("\n");
-    var headers=lines[0].split(",");
-    for(var i=1;i<lines.length;i++){
-      var obj = {};
-      var currentline=lines[i].split(",");
-      for(var j=0;j<headers.length;j++){
-        obj[headers[j]] = currentline[j];
-      }
-      result.push(obj);
-    }
-
-
-    label = [];
-    dataL = [];
-    dataC = [];
-    dataOverfit = [];
-    dataUnderfit = [];
-    dataLinearfit = [];
-    for(var i=0;i<result.length-1;i++){
-      label.push(result[i].Day);
-      dataL.push(result[i].num_positive);
-      dataOverfit.push(result[i].num_positive);
-      dataUnderfit.push(result[i].num_positive);
-      dataLinearfit.push(result[i].num_positive);
-      if(i >= 7 ){
-        dataC.push((parseInt(result[i].num_positive) + parseInt(dataC[i-1]) - parseInt(dataL[i-1]) ).toString());
-      }
-      else if (i != 0) {
-        dataC.push((parseInt(result[i].num_positive) + parseInt(dataC[i-1])).toString());
-      }
-      else {
-        dataC.push(result[i].num_positive);
-      }
-    }
-
-
-
-    for(var i=0;i<7;i++){
-      label.push((result.length+i).toString());
-    }
-
-
-
-    tmpOverfit = dataL[result.length-2];
-    tmpUnderfit = dataL[result.length-2];
-    tmpLinearfit = dataL[result.length-2];
-
-    var m = 0;
-    for(var i=1;i<result.length-1;i++){
-        m +=(dataL[i] - dataL[i-1])/((label[i] - label[i-1]));
-    }
-    m/=result.length-1;
-
-    for(var i=result.length-1;i<label.length;i++){
-      tmpLinearfit = m * parseInt(label[i]);
-      if(i%2 == 0){
-        tmpOverfit = 4 + parseInt(tmpLinearfit);
-        tmpUnderfit = parseInt(tmpLinearfit) - 4;
-      }
-      else{
-        tmpOverfit = 3 + parseInt(tmpLinearfit);
-        tmpUnderfit = parseInt(tmpLinearfit)-3;
-      }
-
-
-      dataLinearfit[i]= tmpLinearfit.toString();
-      dataOverfit[i]= tmpOverfit.toString()*1.1;
-      dataUnderfit[i]= tmpUnderfit.toString()/1.1;
-
-      pred_cum = (parseInt(tmpLinearfit) + parseInt(dataC[i-1]) - parseInt(dataLinearfit[i-7]))
-
-      if ( pred_cum >=  0 ){
-        dataC.push(pred_cum.toString());
-      }
-    }
-
-
-
-  var ctx = document.getElementById("myAreaChart");
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -139,7 +50,7 @@ function loadData(){
           pointHoverBorderColor: "rgba(34,139,34, 0.1)",
           pointHitRadius: 10,
           pointBorderWidth: 4,
-          data: cum_cases(1,dataC),
+          data: [],
         },
         // current cases
         {
@@ -281,6 +192,275 @@ function loadData(){
       }
     }
   },);
+
+
+}
+
+function cum_cases_add() {
+  // ctx.clear();
+  ctx = document.getElementById("myAreaChart");
+
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: label,
+      datasets: [
+        //cumulatives cases
+        {
+          label: "Cumulative ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(34,139,34, 0.2)",
+          borderColor: "rgba(34,139,34, 0.1)",
+          pointRadius: 4,
+          pointBackgroundColor: "rgba(34,139,34, 0.1)",
+          pointBorderColor: "rgba(34,139,34, 0.1)",
+          pointHoverRadius: 4,
+          pointHoverBackgroundColor: "rgba(34,139,34, 0.1)",
+          pointHoverBorderColor: "rgba(34,139,34, 0.1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 4,
+          data: dataC,
+        },
+        // current cases
+        {
+          label: "Current ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(78, 115, 223, 0.2)",
+          borderColor: "rgba(78, 115, 223, 1)",
+          pointRadius: 4,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 4,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 4,
+          data: dataL,
+        },
+        //Underfit line
+        {
+          label: "Underfit ",
+          lineTension: 0.1,
+          backgroundColor: "rgba(255,255,255,1)",
+          borderColor: "rgba(255, 193, 7,0.1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(255, 193, 7,1)",
+          pointBorderColor: "rgba(255, 193, 7,1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(255, 193, 7, 1)",
+          pointHoverBorderColor: "rgba(255, 193, 7, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          data: dataUnderfit,
+        },
+        // linear line
+        {
+          label: "Linear ",
+          lineTension: 0.3,
+          backgroundColor: "rgba(255, 193, 7,0.1)",
+          borderColor: "rgba(237, 0, 59, 1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointBorderColor: "rgba(237, 0, 59, 1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointHoverBorderColor: "rgba(237, 0, 59, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          data: dataLinearfit,
+        },
+        //Overfitt line
+        {
+          label: "Overfit ",
+          lineTension: 0.1,
+          backgroundColor: "rgba(255, 193, 7,0.3)",
+          borderColor: "rgba(255, 193, 7,0.1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(255, 193, 7,1)",
+          pointBorderColor: "rgba(255, 193, 7,1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(255, 193, 7, 1)",
+          pointHoverBorderColor: "rgba(255, 193, 7, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          data: dataOverfit,
+        }
+
+
+
+
+    ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 7,
+            // callback: function(value, index, values) {
+            //   return number_format(value)+ ' Days';
+            // }
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            maxTicksLimit: 5,
+            padding: 10,
+
+            callback: function(value, index, values) {
+              return number_format(value)+ ' Cases';
+            }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + number_format(tooltipItem.yLabel)+ ': Cases';
+          }
+        }
+      }
+    }
+  },);
+
+
+}
+
+
+function cum_cases(ans, dataC){
+  if(ans == 1){
+    return dataC;
+  }else{
+    return [];
+  }
+
+}
+function loadData(){
+  var url = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv"
+  var data = ''
+  // DAp
+  var tmp ;
+  $.get(url,function(data){
+    var result = [];
+    var lines=data.split("\n");
+    var headers=lines[0].split(",");
+    for(var i=1;i<lines.length;i++){
+      var obj = {};
+      var currentline=lines[i].split(",");
+      for(var j=0;j<headers.length;j++){
+        obj[headers[j]] = currentline[j];
+      }
+      result.push(obj);
+    }
+
+
+    label = [];
+    dataL = [];
+    dataC = [];
+    dataOverfit = [];
+    dataUnderfit = [];
+    dataLinearfit = [];
+    for(var i=0;i<result.length-1;i++){
+      label.push(result[i].Day);
+      dataL.push(result[i].num_positive);
+      dataOverfit.push(result[i].num_positive);
+      dataUnderfit.push(result[i].num_positive);
+      dataLinearfit.push(result[i].num_positive);
+      if(i >= 7 ){
+        dataC.push((parseInt(result[i].num_positive) + parseInt(dataC[i-1]) - parseInt(dataL[i-1]) ).toString());
+      }
+      else if (i != 0) {
+        dataC.push((parseInt(result[i].num_positive) + parseInt(dataC[i-1])).toString());
+      }
+      else {
+        dataC.push(result[i].num_positive);
+      }
+    }
+
+
+
+    for(var i=0;i<7;i++){
+      label.push((result.length+i).toString());
+    }
+
+
+
+    tmpOverfit = dataL[result.length-2];
+    tmpUnderfit = dataL[result.length-2];
+    tmpLinearfit = dataL[result.length-2];
+
+    var m = 0;
+    for(var i=1;i<result.length-1;i++){
+        m +=(dataL[i] - dataL[i-1])/((label[i] - label[i-1]));
+    }
+    m/=result.length-1;
+
+    for(var i=result.length-1;i<label.length;i++){
+      tmpLinearfit = m * parseInt(label[i]);
+      if(i%2 == 0){
+        tmpOverfit = 4 + parseInt(tmpLinearfit);
+        tmpUnderfit = parseInt(tmpLinearfit) - 4;
+      }
+      else{
+        tmpOverfit = 3 + parseInt(tmpLinearfit);
+        tmpUnderfit = parseInt(tmpLinearfit)-3;
+      }
+
+
+      dataLinearfit[i]= tmpLinearfit.toString();
+      dataOverfit[i]= tmpOverfit.toString()*1.1;
+      dataUnderfit[i]= tmpUnderfit.toString()/1.1;
+
+      pred_cum = (parseInt(tmpLinearfit) + parseInt(dataC[i-1]) - parseInt(dataLinearfit[i-7]))
+
+      if ( pred_cum >=  0 ){
+        dataC.push(pred_cum.toString());
+      }
+    }
+
+
+
+  var ctx = document.getElementById("myAreaChart");
+  without_cum_cases();
 },
 );
 
