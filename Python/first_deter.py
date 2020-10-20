17b80b93fc41b73818dc381136da658d89125ec1
francoislievens <francoislievens@outlook.com>
2020-10-20 16:24:29 +0200

    update

--- Python/first_deter.py
+++ Python/first_deter.py
@@ -15,8 +15,8 @@ class SIR_model():
         """
         Init the model
         """
-        self.beta = 0.5
-        self.gamma = 0.5
+        self.beta = 0.01
+        self.gamma = 0.04
 
     def set_beta(self, beta_value):
         """
@@ -36,34 +36,64 @@ class SIR_model():
         """
         pass
 
-    def SIR(self,S0,I0,R0,t0, t1):
+    def predict(self, S0, I0, R0, t0, t1):
         """
         SIR model:
         """
-        N=S0+R0+I0
-        S=S0; R=R0; I=I0
-        SS=[S0];RR=[R0]; II=[I0]
-        tt=[t0]
-        dt=0.1
-        t=t0
+        N = S0 + R0 + I0
+        S = S0
+        R = R0
+        I = I0
+        # Output list
+        SS = [S0]
+        RR = [R0]
+        II = [I0]
+        tt = [t0]
+        t = t0
         while t <= t1:
-            dS=-self.beta*S*I/N
-            dI=self.beta*S*I/N-self.gamma*I
-            dR=self.gamma*I
-            S=S+dt*dS
-            I=I+dt*dI
-            R=R+dt*dR
-            SS.append(S); II.append(I); RR.append(R)
-            t=t+dt
+            dS = - (self.beta * (I * S)/N) * S
+            dR = self.gamma * I
+            dI = (-dS) - dR
+            print("time {}, I = {}, S= {}, R={}".format(t, I, S, R))
+            S = S + dS
+            I = I + dI
+            R = R + dR
+            SS.append(S)
+            II.append(I)
+            RR.append(R)
+            t = t + 1
             tt.append(t)
-        return(SS,II,RR,tt)
+        return SS,II,RR,tt
 
 
 if __name__ == "__main__":
+
+    # Import datas
+    data = pd.read_csv('git_data.csv', sep=',', header=0)
     """
     make first sir pred:
     """
+    # Store datas:
+    t_0 = 0
+    t_f = 50
+    I_0 = 1
+    S_0 = 999
+    R_0 = 0
     model = SIR_model()
+    # Make predictions:
+    S, I, R, t = model.predict(S_0, I_0, R_0, t_0, t_f)
+
+    plt.plot(t, S, c="green")
+    plt.show()
+    plt.plot(t, I, c="red")
+    plt.show()
+    plt.plot(t, R, c="blue")
+    plt.show()
+
+    print(S)
+    print(I)
+    print(R)
+
 
     pass
 
@@ -78,5 +108,5 @@ if __name__ == "__main__":
 
 
 
-data = pd.read_csv('git_data.csv', sep=',', header=0)
-print(data)
+
+
