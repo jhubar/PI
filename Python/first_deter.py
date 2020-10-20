227229ace732be65027c6ba6354526f0a965a407
francoislievens <francoislievens@outlook.com>
2020-10-20 22:03:38 +0200

    update

--- Python/first_deter.py
+++ Python/first_deter.py
@@ -19,8 +19,8 @@ class SIR_model():
         """
         Init the model
         """
-        self.beta = 1
-        self.gamma = 0.69
+        self.beta = 0.2
+        self.gamma = 0.012
 
     def set_beta(self, beta_value):
         """
@@ -34,36 +34,41 @@ class SIR_model():
         """
         self.gamma = gamma_value
 
-    def fit(self, dataset):
+    def fit(self, dataset, beta_min=0, beta_max=1, gamma_min=0, gamma_max=1, range_size=100, pop_size=1000000):
         """
         Find optimal value of beta and gamma parameters for the given dataset
         """
-        positives = dataset[:, 1]
-        t_zero = dataset[0][0]
-        t_final = dataset[dataset.shape[0]-1][0]
-        range_size = 100
-        pop_size = 1000000
-        # Creation d'une matrice avec toutes les valeurs possibles:
+
+        # Create a matrix to store all SSE values for each tested beta and gamma combinations
         SSE = np.zeros((range_size, range_size))
+        # Set beta and gamma values to test
         gamma_range = []
         beta_range = []
-        interval = 1 / range_size
+        beta_interval = (beta_max - beta_min) / range_size
+        gamma_interval = (gamma_max - gamma_min) / range_size
+        # Store minimal value in tested values
         min = (99999999, 0, 0)
-        value = 0
+        tmp_beta = beta_min
+        tmp_gamma = gamma_min
+        #beta_range.append(tmp_beta)
+        #gamma_range.append(tmp_gamma)
+        # Fill the two vectors with values of beta and gamma to compute
         for i in range(0, range_size):
-            value += interval
-            gamma_range.append(value)
-            beta_range.append(value)
-
-        for i in range(0, range_size):
-            for j in range(0, range_size):
-                # make predictions:
-                DDI = self.predict_perso(pop_size, 1, 0, 1, dataset.shape[0] - 1, beta_range[i], gamma_range[j])
-
-                for k in range(0, len(DDI)):
-                    SSE[i][j] += (DDI[k] - dataset[k][1])**2
+            tmp_beta += beta_interval
+            tmp_gamma += gamma_interval
+            gamma_range.append(tmp_gamma)
+            beta_range.append(tmp_beta)
+        # Performe each simulations
+        for i in range(0, range_size):  # Beta pour les lignes
+            for j in range(0, range_size):  # Gamma pour les colonnes
+                # compute the number of people infected each days
+                infect_in_day = self.predict(pop_size-1, 1, 0, 1, dataset.shape[0], conta_curve=True,
+                                             beta=beta_range[i], gamma=gamma_range[j])
+                for k in range(0, dataset.shape[0]):
+                    SSE[i][j] += (infect_in_day[k] - dataset[k][1])**2
                 if SSE[i][j] <= min[0]:
                     min = (SSE[i][j], i, j)
+            print(i)
 
         #Export matrix:
         savetxt('see_matrix.csv', SSE, delimiter=",")
@@ -88,89 +93,49 @@ class SIR_model():
 
         pass
 
-    def predict(self, S0, I0, R0, t0, t1):
-        N = S0 + R0 + I0;
-        S = S0;
-        R = R0;
-        I = I0;
-        SS = [S0];
-        RR = [R0];
-        II = [I0];
-        tt = [t0];
-        dt = 1;
-        t = t0
-        DDI = []
-        DDI.append(0)
-        while t <= t1:
-            dS = -self.beta * S * I / N
-            dI = self.beta * S * I / N - self.gamma * I
-            dR = self.gamma * I
-            DDI.append(self.beta * S * I / N)
-            S = S + dt * dS;
-            I = I + dt * dI;
-            R = R + dt * dR;
-            SS.append(S);
-            II.append(I);
-            RR.append(R)
-            t = t + dt;
-            tt.append(t)
-        return (SS, II, RR, tt, DDI)
-
-    def predict_perso(self, S0, I0, R0, t0, t1, beta, gamma):
-        N = S0 + R0 + I0;
-        S = S0;
-        R = R0;
-        I = I0;
-        SS = [S0];
-        RR = [R0];
-        II = [I0];
-        tt = [t0];
-        dt = 1;
-        t = t0
-        DDI = []
-        while t <= t1:
-            dS = -beta * S * I / N
-            dI = beta * S * I / N - gamma * I
-            DDI.append(beta * S * I / N)
-            dR = gamma * I
-            S = S + dt * dS;
-            I = I + dt * dI;
-            R = R + dt * dR;
-            SS.append(S);
-            II.append(I);
-            RR.append(R)
-            t = t + dt;
-            tt.append(t)
-        return DDI
-
-    def predict_old(self, S0, I0, R0, t0, t1):
-        """
-        SIR model:
-        """
+    def predict(self, S0, I0, R0, t0, t1, conta_curve=False, beta=-1, gamma=-1):
+        # If beta or gama are given
+        if beta == -1:
+            beta_val = self.beta
+        else:
+            beta_val = beta
+        if gamma == -1:
+            gamma_val = -1
+        else:
+            gamma_val = gamma
+        # Repartition of population
         N = S0 + R0 + I0
         S = S0
         R = R0
         I = I0
-        # Output list
         SS = [S0]
         RR = [R0]
         II = [I0]
         tt = [t0]
         t = t0
+        contaminations = []
+        contaminations.append(0)
         while t <= t1:
-            dS = - (self.beta * (I * S)/N) * S
-            dR = self.gamma * I
-            dI = (-dS) - dR
-            print("time {}, I = {}, S= {}, R={}".format(t, I, S, R))
-            S = S + dS
-            I = I + dI
-            R = R + dR
+            dS = float(-(beta_val * S * I / N))
+            #dI = float((beta_val * S * I / N) - gamma_val * I)
+            dR = float(gamma_val * I)
+            dI = (-1)*dS + dR
+            contaminations.append(beta_val * S * I / N)
+            S += dS
+            I += dI
+            R += dR
             SS.append(S)
             II.append(I)
             RR.append(R)
-            t = t + 1
+            t += 1
             tt.append(t)
-        return SS,II,RR,tt
+
+        # If we want contamination curve:
+        if conta_curve:
+            return contaminations
+        return SS, II, RR, tt
+
+
 
 
 if __name__ == "__main__":
@@ -183,27 +148,26 @@ if __name__ == "__main__":
     """
     # Store datas:
     t_0 = 0
-    t_f = 500
+    t_f = 30
     I_0 = 1
     S_0 = 999999
     R_0 = 0
     model = SIR_model()
     # Make predictions:
-    S, I, R, t, DDI = model.predict(S_0, I_0, R_0, t_0, t_f)
+    S, I, R, t = model.predict(S_0, I_0, R_0, t_0, t_f)
+    DDI = model.predict(S_0, I_0, R_0, t_0, t_f, conta_curve=True)
 
     plt.plot(t, S, c="green")
-    plt.plot(t, I, c="red")
-    plt.plot(t, R, c="blue")
+
+    #plt.plot(t, I, c="red")
+    #plt.plot(t, R, c="blue")
     plt.show()
 
     plt.plot(t, DDI)
     plt.show()
 
-    model.fit(data_matrix)
+    model.fit(data_matrix, beta_min=0.15, beta_max=0.3, gamma_min=0.01, gamma_max=0.1, range_size=500)
 
-    for i in range(0, 25):
-        print(DDI[i])
-        print(data_matrix[i][1])
 
 
     pass
