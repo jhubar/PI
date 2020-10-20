74893417f837d626a54d512618a1fb495af37b9e
Hubar Julien <Julien1941@live.be>
2020-10-20 13:09:57 +0200

    udpate index

--- js/demo/chart-area-Hospitalised.js
+++ js/demo/chart-area-Hospitalised.js
@@ -1,7 +1,9 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
+const rk4 = require("ode-rk4");
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 loadData();
+
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
