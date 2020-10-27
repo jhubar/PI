3824dfb6188bc12d5be493c1249bb7e6a252cd24
francoislievens <francoislievens@outlook.com>
2020-10-27 18:44:30 +0100

    Update SEIR_extended.py

--- Python/SEIR_extended.py
+++ Python/SEIR_extended.py
@@ -104,9 +104,9 @@ class SEIR():
 
     def fit_hcr(self, dataset):
         # Set initial state:
-        H_0 = df_np[0][7]
-        E_0 = 3 * df_np[1][1]  # Vu qu'un tiers de ce nombre devront être positifs à t+1
-        I_0 = df_np[0][1] - H_0  # Les hospitalisés ne participent plus à la contagion
+        H_0 = dataset[0][7]
+        E_0 = 3 * dataset[1][1]  # Vu qu'un tiers de ce nombre devront être positifs à t+1
+        I_0 = dataset[0][1] - H_0  # Les hospitalisés ne participent plus à la contagion
         S_0 = 999999 - H_0 - I_0 - E_0
         R_0 = 0
         initial_state = (S_0, E_0, I_0, H_0, R_0)
@@ -130,6 +130,33 @@ class SEIR():
         plt.yscale('log')
         plt.xlabel('hcr value')
         plt.show()
+
+    def fit_scipy(self, dataset):
+        """
+        On commence par fitter les paramètres beta (dans un interval de 0 à 1, pas d'indications pour lui),
+        gamma dans un interval de 1/10 à 1/4 donné dans l'énnoncé, et sigma dans un interval de 1/5 à 1
+        (également donné).
+        Nous fittons les données de testage cummulées sur la somme des courbes I, H et R.
+
+        """
+        time = dataset[:, 0]
+        # Set initial state:
+        initial_state = (1000000 - dataset[0][1] - 3*dataset[2][1], 3 * dataset[2][1], dataset[0][1], dataset[0][4], 0)
+        start_values = [self.beta, self.gamma, self.sigma, self.hp, self.hcr]
+        bounds = [(0, 1), (1/10, 1/4), (1/5, 1), (self.hp, self.hp), (self.hcr, self.hcr)]
+        res = minimize(self.SSE, np.asarray(start_values), args=(initial_state, time, dataset, 'fit_on_cumul_positive'),
+                       method='L-BFGS-B', bounds=bounds)
+        print(res)
+        self.beta = res.x[0]
+        self.gamma = res.x[1]
+        self.sigma = res.x[2]
+        """
+        Deuxième étape, il ne reste plus qu'à fitter hcr sur la courbe des hospitalisations (non cumulées)
+        NOTE : Je ne sais pas pourquoi, mais ça converge pas quand j'utilise minimize alors que ça converge très bien
+        en itérant toutes les valeurs
+        """
+        self.fit_hcr(dataset)
+
 
     def SSE(self, parameters, initial_state, time, data, method='fit_on_hospit'):
 
@@ -164,6 +191,7 @@ class SEIR():
 
 
 
+
 def plot_predict_and_compare(df, pred, args='predict'):
 
     if 'predict' in args:
@@ -183,8 +211,8 @@ def plot_predict_and_compare(df, pred, args='predict'):
         cumul_positive = []
         hospit = []
         for i in range(0, len(df['Day'].to_numpy())):
-            cumul_positive.append(predictions[i][3] + predictions[i][4] + predictions[i][5])  # somme de I, H et R
-            hospit.append(predictions[i][4])
+            cumul_positive.append(pred[i][3] + pred[i][4] + pred[i][5])  # somme de I, H et R
+            hospit.append(pred[i][4])
         plt.plot(df['Day'], cumul_positive, c='red')
         plt.plot(df['Day'], hospit, c='blue')
         if "log" in args:
@@ -192,10 +220,7 @@ def plot_predict_and_compare(df, pred, args='predict'):
         plt.show()
 
 
-
-
-if __name__ == "__main__":
-
+def first_method():
     """
     Import the dataset and add informations
     """
@@ -206,7 +231,7 @@ if __name__ == "__main__":
     # Insert cumul_positive column at the end
     cumul_positive = df["num_positive"].to_numpy()
     for i in range(1, len(cumul_positive)):
-        cumul_positive[i] += cumul_positive[i-1]
+        cumul_positive[i] += cumul_positive[i - 1]
     df.insert(7, "cumul_positive", cumul_positive)
     # print(df)
     # Make a numpy version of the dataframe:
@@ -215,18 +240,17 @@ if __name__ == "__main__":
     model = SEIR()
     # Set initial state
     H_0 = df_np[0][7]
-    E_0 = 3 * df_np[1][1]                     # Vu qu'un tiers de ce nombre devront être positifs à t+1
-    I_0 = df_np[0][1] - H_0                   # Les hospitalisés ne participent plus à la contagion
+    E_0 = 3 * df_np[1][1]  # Vu qu'un tiers de ce nombre devront être positifs à t+1
+    I_0 = df_np[0][1] - H_0  # Les hospitalisés ne participent plus à la contagion
     S_0 = 999999 - H_0 - I_0 - E_0
     R_0 = 0
-
 
     """ *****************************************************************************
         ETAPE 1: 
         Paramètres donnés par le prof: 
             - Gamma: 1/7
             - Sigma: 1/3
-        
+
         Première étape = déterminer la relation entre les hospitalisations et les infectés
         Pour ça on analyse la proportion moyenne entre la courbe cumulée des tests positifs
         et la courbe cumulée des hospitalisés. 
@@ -239,14 +263,14 @@ if __name__ == "__main__":
     posit = df['cumul_positive'].to_numpy()
     hospit = df['num_cumulative_hospitalizations'].to_numpy()
     factor = []
-    for i in range(15, len(posit)): # Begin after 15 days because stabilisation of the rapport
-        factor.append(posit[i]/hospit[i])
+    for i in range(15, len(posit)):  # Begin after 15 days because stabilisation of the rapport
+        factor.append(posit[i] / hospit[i])
     factor = np.array(factor)
     # Predict positive curve from this:
     predict_cumul_positive = df['num_cumulative_hospitalizations'].to_numpy()
     predict_cumul_positive = predict_cumul_positive * np.mean(factor)
     # Set the value to the model.
-    model.set_hospit_prop(1/np.mean(factor))
+    model.set_hospit_prop(1 / np.mean(factor))
 
     print('"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""')
     print(" Analyze of the proportion of hospitalized in the positives case")
@@ -291,5 +315,92 @@ if __name__ == "__main__":
     predictions = model.predict(S_0, E_0, I_0, H_0, R_0, duration=50)
 
     plot_predict_and_compare(df, predictions, args='predict compare no_S log')
+
+
+def sec_method():
+
+    """
+    Import the dataset and add informations
+    """
+    url = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv"
+    df = pd.read_csv(url, sep=",", header=0)
+    # Delete the first line:
+    df = df.drop([0], axis=0)
+    # Insert cumul_positive column at the end
+    cumul_positive = df["num_positive"].to_numpy()
+    for i in range(1, len(cumul_positive)):
+        cumul_positive[i] += cumul_positive[i - 1]
+    df.insert(7, "cumul_positive", cumul_positive)
+    # print(df)
+    # Make a numpy version of the dataframe:
+    df_np = df.to_numpy()
+    # Init the model:
+    model = SEIR()
+    # Set initial state
+    H_0 = df_np[0][7]
+    E_0 = 3 * df_np[1][1]  # Vu qu'un tiers de ce nombre devront être positifs à t+1
+    I_0 = df_np[0][1] - H_0  # Les hospitalisés ne participent plus à la contagion
+    S_0 = 999999 - H_0 - I_0 - E_0
+    R_0 = 0
+
+
+
+    """ *****************************************************************************
+        ETAPE 1: 
+        Trouver le paramètre hp, de la même façon que dans methode 1
+        *****************************************************************************
+    """
+    # Find the linear relation between the two curves:
+    posit = df['cumul_positive'].to_numpy()
+    hospit = df['num_cumulative_hospitalizations'].to_numpy()
+    factor = []
+    for i in range(15, len(posit)):  # Begin after 15 days because stabilisation of the rapport
+        factor.append(posit[i] / hospit[i])
+    factor = np.array(factor)
+    # Predict positive curve from this:
+    predict_cumul_positive = df['num_cumulative_hospitalizations'].to_numpy()
+    predict_cumul_positive = predict_cumul_positive * np.mean(factor)
+    # Set the value to the model.
+    model.set_hospit_prop(1 / np.mean(factor))
+    print('"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""')
+    print(" Analyze of the proportion of hospitalized in the positives case")
+    print(" Average = {}".format(np.mean(factor)))
+    print(" Standard deviation = {}".format(np.std(factor)))
+    plt.plot(df['Day'], df['cumul_positive'], c='blue', label='Cumul_positive')
+    plt.plot(df['Day'], df['num_cumulative_hospitalizations'], c='red', label='Cumul_hospit')
+    plt.plot(df['Day'], predict_cumul_positive, c='green', label='Predicted positives from hospit')
+    plt.ylabel('nb people')
+    plt.xlabel('time in days')
+    plt.title('Compare cumul positive and cumul hospit in dataset')
+    plt.show()
+
+
+
+    """ *****************************************************************************
+        ETAPE 2: 
+        On fit le modèle: 
+            - D'abord les paramètres beta, gamma et sigma. Le paramètre hp a été fixé
+                à l'étape 1 et le paramètre hcr est fixé à zéro car n'intervient pas
+                dans le cas ou l'on fit sur les courbes I+R+H 
+            - Après on fit hcr sur la courbe des hospitalisations.
+        *****************************************************************************
+    """
+    model.fit_scipy(df_np)
+
+    """ *****************************************************************************
+        Nous pouvons maintenant comparer les simulations et les données ainsi que 
+        dessiner des prédictions à long terme. 
+        *****************************************************************************
+    """
+    predictions = model.predict(S_0, E_0, I_0, H_0, R_0, duration=150)
+    plot_predict_and_compare(df, predictions, args='predict compare')
+
+    predictions = model.predict(S_0, E_0, I_0, H_0, R_0, duration=50)
+
+    plot_predict_and_compare(df, predictions, args='predict compare no_S log')
+
+if __name__ == "__main__":
+
+    sec_method()
 
 
