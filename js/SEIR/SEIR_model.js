a48bf4b51668c83b440ef761ec0bab7fd2a60f7e
Hubar Julien <Julien1941@live.be>
2020-11-03 18:34:00 +0100

    update

--- js/SEIR/SEIR_model.js
+++ js/SEIR/SEIR_model.js
@@ -60,6 +60,12 @@ function loadData(){
       data_seir_r = [];
       data_seir_h = [];
 
+      data_day_fit = [];
+      data_seir_fit_cumul_pos = [];
+      data_seir_fit_hospit = [];
+      data_seir_fit_cumul_pos_fit = [];
+      data_seir_fit_hospit_fit = [];
+
 
 
       for(var i=0;i<$value_time_SEIR.val();i++){
@@ -74,9 +80,22 @@ function loadData(){
 
       }
 
+      for(var i=0;i<result.log.length-1;i++){
+
+        data_day_fit.push(result.log[i].day);
+        data_seir_fit_cumul_pos.push(result.log[i].cumul_positive);
+        data_seir_fit_hospit.push(result.log[i].hospit);
+        data_seir_fit_cumul_pos_fit.push(result.log[i].cumul_positive_fit);
+        data_seir_fit_hospit_fit.push(result.log[i].hospit_fit);
+
+
+      }
+
+
 
 
   var ctx_active_cases = document.getElementById("myAreaSeirModel");
+  var ctx_active_cases_fit = document.getElementById("myAreaSeirfit");
   $("#id_beta_seir").html((parseFloat(result.model[0].beta).toFixed(6)).toString())
   $("#id_sigma_seir").html((parseFloat(result.model[0].sigma).toFixed(6)).toString())
   $("#id_gamma_seir").html((parseFloat(result.model[0].gamma).toFixed(6)).toString())
@@ -86,6 +105,7 @@ function loadData(){
 
 
   draw();
+  drawfit();
   load_card_value();
 
 
@@ -281,6 +301,166 @@ function draw(ans) {
           pointBorderWidth: 4,
           data: hospitalized_draw(),
         }
+
+
+
+
+
+    ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 7,
+            // callback: function(value, index, values) {
+            //   return number_format(value)+ ' Days';
+            // }
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            maxTicksLimit: 5,
+            padding: 10,
+
+            callback: function(value, index, values) {
+              return number_format(value)+ ' Cases';
+            }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + number_format(tooltipItem.yLabel)+ ': Cases';
+          }
+        }
+      }
+    }
+  },);
+
+
+}
+
+function drawfit(ans) {
+
+  if (typeof(myLineChartfit) != "undefined"){
+
+    myLineChartfit.destroy();
+
+  }
+
+  ctx_active_cases_fit = document.getElementById("myAreaSeirfit");
+  myLineChartfit = new Chart(ctx_active_cases_fit, {
+    type: 'line',
+    data: {
+      labels: data_day_fit,
+      datasets: [
+        // Susceptible
+        {
+          label: "Susceptible ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(78, 115, 223, 0.2)",
+          borderColor: "rgba(78, 115, 223, 1)",
+          pointRadius: 1,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 1,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 5,
+          pointBorderWidth: 1,
+          data: data_seir_fit_hospit_fit,
+        },
+        // Exposed
+        {
+          label: "Exposed ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(240, 173, 78, 0.2)",
+          borderColor: "rgba(240, 173, 78, 1)",
+          pointRadius: 1,
+          pointBackgroundColor: "rgba(240, 173, 78, 1)",
+          pointBorderColor: "rgba(240, 173, 78, 1)",
+          pointHoverRadius: 1,
+          pointHoverBackgroundColor: "rgba(240, 173, 78, 1)",
+          pointHoverBorderColor: "rgba(240, 173, 78, 1)",
+          pointHitRadius: 5,
+          pointBorderWidth: 1,
+          data: data_seir_fit_hospit,
+        },
+        // Infectious
+        {
+          label: "Infectious ",
+          lineTension: 0.3,
+          backgroundColor: "rgba(237, 0, 59,0.1)",
+          borderColor: "rgba(237, 0, 59, 1)",
+          pointRadius: 1,
+          pointBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointBorderColor: "rgba(237, 0, 59, 1)",
+          pointHoverRadius: 1,
+          pointHoverBackgroundColor: "rgba(237, 0, 59, 1)",
+          pointHoverBorderColor: "rgba(237, 0, 59, 1)",
+          pointHitRadius: 5,
+          pointBorderWidth: 1,
+          data: data_seir_fit_cumul_pos,
+        },
+        // Recovered
+        {
+          label: "Recovered ",
+          lineTension: 0.6,
+          backgroundColor: "rgba(37, 56, 60, 0.2)",
+          borderColor: "rgba(37, 56, 60, 0.1)",
+          pointRadius: 1,
+          pointBackgroundColor: "rgba(37, 56, 60, 0.1)",
+          pointBorderColor: "rgba(37, 56, 60, 0.1)",
+          pointHoverRadius: 1,
+          pointHoverBackgroundColor: "rgba(37, 56, 60, 0.1)",
+          pointHoverBorderColor: "rgba(37, 56, 60, 0.1)",
+          pointHitRadius: 5,
+          pointBorderWidth: 1,
+          data: data_seir_fit_cumul_pos_fit,
+        },
+
 
 
 
