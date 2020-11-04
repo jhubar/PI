2f8bd53206eaa0a191058a92d5c7bcb13ea547b5
Hubar Julien <Julien1941@live.be>
2020-11-04 15:15:01 +0100

    update

--- js/Network/network.js
+++ js/Network/network.js
@@ -1,35 +1,56 @@
-anychart.onDocumentReady(function () {
-        anychart.data.loadJsonFile("https://static.anychart.com/git-storage/word-press/data/network-graph-tutorial/data_images.json", function (data) {
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+  width = 400 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
-          // create a chart from the loaded data
-          var chart = anychart.graph(data);
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-          // set the title
-          chart.title("Network Graph showing the battles in Game of Thrones");
+d3.json("https://raw.githubusercontent.com/julien1941/PI/master/js/Network/dataNet.json?token=AL3RLGNUBAWWLE5W7IHSTQK7VPYVG", function( data) {
 
-          // access nodes
-          var nodes = chart.nodes();
+  // Initialize the links
+  var link = svg
+    .selectAll("line")
+    .data(data.links)
+    .enter()
+    .append("line")
+      .style("stroke", "#aaa")
 
-          // set the size of nodes
-          nodes.normal().height(30);
-          nodes.hovered().height(45);
-          nodes.selected().height(45);
+  // Initialize the nodes
+  var node = svg
+    .selectAll("circle")
+    .data(data.nodes)
+    .enter()
+    .append("circle")
+      .attr("r", 20)
+      .style("fill", "#69b3a2")
 
-          // set the stroke of nodes
-          nodes.normal().stroke(null);
-          nodes.hovered().stroke("#333333", 3);
-          nodes.selected().stroke("#333333", 3);
+  // Let's list the force we wanna apply on the network
+  var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+      .force("link", d3.forceLink()                               // This force provides links between nodes
+            .id(function(d) { return d.id; })                     // This provide  the id of a node
+            .links(data.links)                                    // and this the list of links
+      )
+      .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+      .on("end", ticked);
 
-          // enable the labels of nodes
-          chart.nodes().labels().enabled(true);
+  // This function is run at each iteration of the force algorithm, updating the nodes position.
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
-          // configure the labels of nodes
-          chart.nodes().labels().format("{%id}");
-          chart.nodes().labels().fontSize(12);
-          chart.nodes().labels().fontWeight(600);
+    node
+         .attr("cx", function (d) { return d.x+6; })
+         .attr("cy", function(d) { return d.y-6; });
+  }
 
-          // draw the chart
-          chart.container("container1").draw();
-
-        });
-      });
+});
