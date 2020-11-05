c5f49dce16dc5510ea04b3d242f2bcadd2b48a02
francoislievens <francoislievens@outlook.com>
2020-11-05 14:08:32 +0100

    big correction

--- Python/SEHR+.py
+++ Python/SEHR+.py
@@ -8,7 +8,7 @@ import math
 
 """
 =======================================================================================================
-SEIHR MODEL
+SEIHR +  MODEL
 =======================================================================================================
 """
 
@@ -18,11 +18,14 @@ class SEIR():
     def __init__(self):
 
         # Model's hyperparameters
-        self.beta = None        # Contagion probability
-        self.sigma = None       # Probability to go from E to I
-        self.gamma = None       # Probability parameter to go from I to R (to be cure)
-        self.hp = None          # Probability to go from I to H
+        self.beta = 0        # Contagion probability
+        self.sigma = 0       # Probability to go from E to I
+        self.gamma = 0       # Probability parameter to go from I to R (to be cure)
+        self.hp = 0             # Probability to go from I to H
         self.hcr = 0            # Hospit Cure Rate
+        self.pc = 0             # Probability to fall in ICU each day from H
+        self.pd = 0             # Probability to die each day in icu
+        self.pcr = 0            # Probability to recover from critical
 
         # Data to fit
         self.raw_dataset = None # Original dataset, before preprocessing
@@ -34,7 +37,9 @@ class SEIR():
         self.E_0 = None         # Exposed: people in incubation: Can't spread the agent
         self.I_0 = None         # Infectious: people who can spread the disease
         self.H_0 = None         # Hospitalized peoples: can't spread any more the agent
+        self.C_0 = None         # Critical: peoples who are in ICU
         self.R_0 = None         # Recovered people: can't catch again the agent due to immunity
+        self.D_0 = None         # Dead: people who die in ICU
         self.N = None           # The total size of the population
 
         # Data to store
@@ -47,28 +52,30 @@ class SEIR():
         """
         Function who return a tuple with the initial state of the model
         """
-        return (self.S_0, self.E_0, self.I_0, self.H_0, self.R_0, self.N)
+        return (self.S_0, self.E_0, self.I_0, self.H_0, self.R_0, self.N, self.C_0, self.D_0)
 
     def get_parameters(self):
         """
         Function who return a tuple with model's hyper-parameters
         """
-        return (self.beta, self.sigma, self.gamma, self.hp, self.hcr)
+        return (self.beta, self.sigma, self.gamma, self.hp, self.hcr, self.pc, self.pd, self.pcr)
 
-    def differential(self, state, time, beta, sigma, gamma, hp, hcr):
+    def differential(self, state, time, beta, sigma, gamma, hp, hcr, pc, pd, pcr):
         """
         Differential equations of the model
         """
-        S, E, I, H, R, N = state
+        S, E, I, H, R, N, C, D = state
 
         dS = -(beta * S * I) / N
         dE = (beta * S * I) / N - E * sigma
         dI = (E * sigma) - (gamma * I) - (hp * I)
-        dH = (hp * I) - (hcr * H)
+        dH = (hp * I) - (hcr * H) - (pc * H)
+        dC = (pc * H) - (pd * C) - (pcr * C)
         dR = (gamma * I) + (hcr * H)
+        dD = (pd * C)
         dN = 0
 
-        return dS, dE, dI, dH, dR, dN
+        return dS, dE, dI, dH, dR, dN, dC, dD
 
     def predict(self, duration):
         """
@@ -86,14 +93,15 @@ class SEIR():
                          t=time,
                          args=parameters)
 
-        return np.vstack((time, predict[:, 0], predict[:, 1], predict[:, 2], predict[:, 3], predict[:, 4])).T
+        return np.vstack((time, predict[:, 0], predict[:, 1], predict[:, 2], predict[:, 3], predict[:, 4],
+                          predict[:, 5], predict[:, 6], predict[:, 7])).T
 
     def fit(self):
         """
         Fitting
         """
         # =========================================================================== #
-        # First part: We fit the parameters beta, sigma and gamma by computing the
+        # PART 1: We fit the parameters beta, sigma and gamma by computing the
         # sum of errors between the product of positive tests and the product of the
         # curves I, H and R. In fact, because our parameters hp is fixed at zero,
         # the curve H = 0
@@ -115,16 +123,49 @@ class SEIR():
         self.sigma = res.x[1]
         self.gamma = res.x[2]
         # =========================================================================== #
-        # Second part: In our model, infectious people can go from I to H and not
+        # PART 2: In our model, infectious people can go from I to H and not
         # only to R. So, we have to split a part of gamma parameter to hp parameter:
         # =========================================================================== #
         self.gamma_hp_slide()
 
         # =========================================================================== #
-        # Third part: We can now fit hcr by computing error between hospitalized
-        # in the dataset and our H curve prediction
+        # PART 3: compute the probability to out of H.
+        # WARNING: this probability contain the probability to be cure and the
+        # probability to fall in ICU
         # =========================================================================== #
         self.fit_hcr()
+
+        # =========================================================================== #
+        # PART 4: People in H state can not only being cured. so we will distribute
+        # the actual value of hcr in hcr (probability to be cured) and pc (probability
+        # to fall in Critical cases). Because we don't have cumulative informations
+        # about critical cases, we have to fit in the same time a probability pcr who
+        # represent the probability to leave the critical state. So, we are optimizing
+        # 1. the ratio of actual hcr who begin pc
+        # 2. the value of pcr
+        # =========================================================================== #
+        # Generate initial state:
+        initial_state = self.get_initial_state()
+        # Time vector:
+        time = self.dataset[:, 0]
+        # Bounds: hcr/pc ratio, pcr
+        bounds = [(0, 1), (0, 1)]
+        # Start values
+        start_values = [0.5, 0.5]
+        # Use Scipy.optimize.minimize with L-BFGS_B method
+        res = minimize(self.SSE, np.asarray(start_values), args=(initial_state, time, 'part_four'),
+                       method='L-BFGS-B', bounds=bounds)
+        print(res)
+        initial_hcr = self.hcr
+        self.hcr = res.x[0] * initial_hcr
+        self.pc = (1 - res.x[0]) * initial_hcr
+        self.pcr = res.x[1]
+
+        # =========================================================================== #
+        # PART 5: We can now slide the actual value of pcr to spare the probability
+        # to be cure from C (definitive pcr) and to die form C (pd)
+        # =========================================================================== #
+        self.pcr_pd_slide()
 
     def gamma_hp_slide(self):
         """
@@ -146,7 +187,7 @@ class SEIR():
         SSE = []
         best = (math.inf, 0, 0)
         for g in range(0, range_size):
-            params = (self.beta, self.sigma, gamma_range[g], hp_range[g], 0)
+            params = (self.beta, self.sigma, gamma_range[g], hp_range[g], 0, 0, 0, 0)
             # Compute the SSE for theses params on the cumulative hospit. curve (because hcr = 0)
             sse = self.SSE(params, initial_state, time, 'second_part')
             SSE.append(sse)
@@ -161,8 +202,46 @@ class SEIR():
 
         #plot :
         plt.plot(gamma_range, SSE, c='blue', label='Gamma value')
-        #plt.yscale('log')
+        plt.yscale('log')
         plt.xlabel('gamma prooportion')
+        plt.show()
+
+    def pcr_pd_slide(self):
+        """
+        This method find definitive value of pcr and pd
+        """
+        # The number of value to test
+        range_size = 1000
+        initial_pcr = self.gamma
+        # Each value of gamma to test
+        pcr_range = np.linspace(0, initial_pcr, range_size)
+        # Corresponding value of hp:
+        pd_range = np.linspace(0, initial_pcr, range_size)
+        pd_range = np.flip(pd_range)
+
+        # Make simulation and compute SSE:
+        initial_state = self.get_initial_state()
+        time = self.dataset[:, 0]
+        SSE = []
+        best = (math.inf, 0, 0)
+        for g in range(0, range_size):
+            params = (self.beta, self.sigma, self.gamma, self.hp, self.hcr, self.pc, pd_range[g], pcr_range[g])
+            # Compute the SSE for theses params on the cumulative hospit. curve (because hcr = 0)
+            sse = self.SSE(params, initial_state, time, 'part_5')
+            SSE.append(sse)
+            if sse < best[0]:
+                best = (sse, pd_range[g], pcr_range[g])
+
+        self.pcr = best[2]
+        self.pd = best[1]
+        print("pd/pcr slide: best value with sse = {}".format(best[0]))
+        print("best pcr = {}".format(self.pcr))
+        print("best pd = {}".format(self.pd))
+
+        #plot :
+        plt.plot(pcr_range, SSE, c='blue', label='pcr value')
+        plt.yscale('log')
+        plt.xlabel('pcr prooportion')
         plt.show()
 
     def fit_hcr(self):
@@ -181,7 +260,7 @@ class SEIR():
         SSE = []
         best = (math.inf, 0)
         for g in range(0, range_size):
-            params = (self.beta, self.sigma, self.gamma, self.hp, hcr_range[g])
+            params = (self.beta, self.sigma, self.gamma, self.hp, hcr_range[g], 0, 0, 0)
             # Compute the SSE for theses params on the normal hospit. curve
             sse = self.SSE(params, initial_state, time, 'third_part')
             SSE.append(sse)
@@ -234,7 +313,7 @@ class SEIR():
         if method == 'first_part':
             # Set parameters: we set hp et hcr to zero
             tpl = tuple(parameters)
-            params = (tpl[0], tpl[1], tpl[2], 0, 0)
+            params = (tpl[0], tpl[1], tpl[2], 0, 0, 0, 0, 0)
 
             # Make predictions:
             predict = odeint(func=self.differential,
@@ -269,7 +348,38 @@ class SEIR():
                              args=params)
             sse = 0.0
             for i in range(0, len(time)):
+                # Error on non-cumulative hospit
                 sse += (self.dataset[i][3] - predict[i][3]) ** 2
+            return sse
+
+        if method == 'part_four':
+
+            tpl = tuple(parameters)
+            params = (self.beta, self.sigma, self.gamma, self.hp,
+                      tpl[0] * self.hcr, (1 - tpl[0]) * self.pc, 0, tpl[1])
+            # Make predictions:
+            predict = odeint(func=self.differential,
+                             y0=initial_state,
+                             t=time,
+                             args=params)
+            sse = 0.0
+            for i in range(0, len(time)):
+                # fit on ICU
+                sse += (self.dataset[i][5] - predict[i][6]) ** 2
+            return sse
+
+        if method == 'part_5':
+
+            params = tuple(parameters)
+            # Make predictions:
+            predict = odeint(func=self.differential,
+                             y0=initial_state,
+                             t=time,
+                             args=params)
+            sse = 0.0
+            for i in range(0, len(time)):
+                # Error on death cases
+                sse += (self.dataset[i][6] - predict[i][7]) ** 2
             return sse
 
     def dataframe_smoothing(self, df):
@@ -354,6 +464,8 @@ class SEIR():
             self.H_0 = self.dataset[0][3]
             self.E_0 = 3 * self.dataset[1][1]    # Because mean of incubation period = 3 days
             self.R_0 = 0
+            self.C_0 = 0
+            self.D_0 = 0
             self.S_0 = self.N - self.I_0 - self.H_0 - self.E_0
 
             # Initialize default value to hyper-parameters:
@@ -395,21 +507,26 @@ class SEIR():
 
         if 'predict' in args:
             if "no_S" not in args:
-                plt.plot(pred[:, 0], pred[:, 1], c='black', label="S")
+                plt.plot(pred[:, 0], pred[:, 1], c='green', label="S")
             plt.plot(pred[:, 0], pred[:, 2], c='yellow', label="E")
             plt.plot(pred[:, 0], pred[:, 3], c='red', label="I")
             plt.plot(pred[:, 0], pred[:, 4], c='purple', label="H")
             plt.plot(pred[:, 0], pred[:, 5], c='blue', label='R')
+            plt.plot(pred[:, 0], pred[:, 7], c='orange', label='C')
+            plt.plot(pred[:, 0], pred[:, 8], c='black', label='D')
             plt.show()
 
         if 'compare' in args:
             plt.scatter(self.dataframe['Day'], self.dataframe['cumul_positive'], c='red')
-            plt.scatter(self.dataframe['Day'], self.dataframe['num_hospitalised'], c='blue')
+            if self.pc == 0 and self.hcr == 0:
+                plt.scatter(self.dataframe['Day'], self.dataframe['num_cumulative_hospitalizations'], c='blue', label='cumul_hosp')
+            else:
+                plt.scatter(self.dataframe['Day'], self.dataframe['num_hospitalised'], c='blue', label='hosp')
 
             cumul_positive = []
             hospit = []
             for i in range(0, len(self.dataframe['Day'].to_numpy())):
-                cumul_positive.append(pred[i][3] + pred[i][4] + pred[i][5])  # somme de I, H et R
+                cumul_positive.append(pred[i][3] + pred[i][4] + pred[i][5] + pred[i][7] + pred[i][8])  # sum of I, H, R, C and D
                 hospit.append(pred[i][4])
 
             print(len(self.dataframe['Day']))
@@ -468,6 +585,9 @@ def first_method():
     print("gamma = {}".format(model.gamma))
     print("hp = {}".format(model.hp))
     print("hcr = {}".format(model.hcr))
+    print('pc = {}'.format(model.hp))
+    print("pcr = {}".format(model.pcr))
+    print("pd = {}".format(model.pd))
     print("=======================================================")
     print("This is a small step for man, but a giant step for COV-Invaders")
 
