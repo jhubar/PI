15c9380cc0498fc4135343889d2aaff794fd639e
jhubar <hubarjulien@gmail.com>
2021-01-12 16:00:43 +0100

    Update chart-area.js

--- js/Final/chart-area.js
+++ js/Final/chart-area.js
@@ -4,7 +4,7 @@ Chart.defaults.global.defaultFontColor = '#858796';
 // const $url_data = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv"
 const $url_data_actu = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv"
 const $url_data = "https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/Cov_invaders.csv"
-const $url = "https://raw.githubusercontent.com/jhubar/PI/master/Python/Data/SEIR.json?token=AL3RLGMJGY6BC4FHPY6FMGC77VGOS"
+const $url = "https://raw.githubusercontent.com/jhubar/PI/master/Python/Data/SEIR%2B.json?token=AL3RLGJBVD55CPUZVZE2GUTAA3Z3Y"
 const $value_time_period_data = $('.value_time_period_data');
 const $value_time_data = $('#range_time_period_data');
 
