5676e5c02f87788a5a5bb8a9ea95ef3fe09bbd63
jhubar <hubarjulien@gmail.com>
2021-03-15 16:32:49 +0100

    update

--- Q2/main.py
+++ Q2/main.py
@@ -11,9 +11,33 @@ def api_covid(countries = None):
         response = requests.get('https://api.covid19api.com/dayone/country/'+str(countries[i]))
         print(response.status_code)
         print(countries[i])
+        if response.status_code != 200:
+            continue
         data_countries = response.json()
         with open('Data/countries'+countries[i]+'.json', 'w') as json_file:
             json.dump(data_countries, json_file)
+        active = []
+        deaths =[]
+        recovered = []
+        confirmed = []
+        date = []
+
+        for i in range (0, len(data_countries)):
+            active.append(data_countries[i]['Active'])
+            deaths.append(data_countries[i]['Deaths'])
+            recovered.append(data_countries[i]['Recovered'])
+            confirmed.append(data_countries[i]['Confirmed'])
+            date.append(datetime.strptime(data_countries[i]['Date'], '%Y-%m-%dT%H:%M:%SZ'))
+
+        # print(confirmed)
+        plt.plot(date,confirmed, label = 'Confirmed')
+        plt.plot(date,deaths, label = 'Deaths')
+        plt.plot(date,recovered, label = 'Active')
+        plt.plot(date,active,label = 'Recovered')
+    plt.legend()
+
+    plt.show()
+    plt.close()
 
 def api_countries():
     response = requests.get('https://api.covid19api.com/countries')
@@ -34,6 +58,7 @@ def load_countries():
     """
     data_countries = pd.read_json('countries.json')
     countries = np.array(data_countries['Country'])
+    print(countries)
     return countries
 
 
