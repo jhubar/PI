52a49e135125939fe971e2677190f7a6d23b4726
jhubar <hubarjulien@gmail.com>
2021-03-21 14:51:55 +0100

    upd

--- Q2/main.py
+++ Q2/main.py
@@ -3,11 +3,18 @@ import numpy as np
 import pandas as pd
 import requests
 import json
+import os
 from datetime import datetime
+
+
 
 def api_covid(countries = None):
 
+
     for i in range(0, len(countries)):
+
+        # if find(name = 'countries'+countries[i]+'.json',path='Data/'):
+        #     print("find: {}".format(countries[i]))
         response = requests.get('https://api.covid19api.com/dayone/country/'+str(countries[i]))
         print(response.status_code)
         print(countries[i])
@@ -29,11 +36,11 @@ def api_covid(countries = None):
             confirmed.append(data_countries[i]['Confirmed'])
             date.append(datetime.strptime(data_countries[i]['Date'], '%Y-%m-%dT%H:%M:%SZ'))
 
-        # print(confirmed)
+
         plt.plot(date,confirmed, label = 'Confirmed')
-        plt.plot(date,deaths, label = 'Deaths')
-        plt.plot(date,recovered, label = 'Active')
-        plt.plot(date,active,label = 'Recovered')
+        plt.plot(date,deaths, label = 'Deaths ')
+        plt.plot(date,recovered, label = 'Active ')
+        plt.plot(date,active,label = 'Recovered ' )
     plt.legend()
 
     plt.show()
@@ -49,7 +56,7 @@ def api_countries():
     countries = []
     for i in range(0,len(data_countries)):
         countries.append(data_countries[i]['Country'])
-    print(countries)
+
     return countries
 
 def load_countries():
@@ -58,36 +65,39 @@ def load_countries():
     """
     data_countries = pd.read_json('countries.json')
     countries = np.array(data_countries['Country'])
-    print(countries)
     return countries
 
 
-def plot():
-    active = []
-    deaths =[]
-    recovered = []
-    confirmed = []
-    date = []
-
-    for i in range (0, len(data)):
-        active.append(data[i]['Active'])
-        deaths.append(data[i]['Deaths'])
-        recovered.append(data[i]['Recovered'])
-        confirmed.append(data[i]['Confirmed'])
-        date.append(datetime.strptime(data[i]['Date'], '%Y-%m-%dT%H:%M:%SZ'))
-
-    # print(confirmed)
-    plt.plot(date,confirmed, label = 'Confirmed')
-    plt.plot(date,deaths, label = 'Deaths')
-    plt.plot(date,recovered, label = 'Active')
-    plt.plot(date,active,label = 'Recovered')
-    plt.legend()
-
-    plt.show()
-    plt.close()
+# def plot_unique_country(country = country):
+#     data_countries = pd.read_json('countries'+ country + '.json')
+#     countries = np.array(data_countries['Country'])
+#     active = []
+#     deaths =[]
+#     recovered = []
+#     confirmed = []
+#     date = []
+#
+#     for i in range (0, len(data)):
+#         active.append(data[i]['Active'])
+#         deaths.append(data[i]['Deaths'])
+#         recovered.append(data[i]['Recovered'])
+#         confirmed.append(data[i]['Confirmed'])
+#         date.append(datetime.strptime(data[i]['Date'], '%Y-%m-%dT%H:%M:%SZ'))
+#
+#     # print(confirmed)
+#     plt.plot(date,confirmed, label = 'Confirmed')
+#     plt.plot(date,deaths, label = 'Deaths')
+#     plt.plot(date,recovered, label = 'Active')
+#     plt.plot(date,active,label = 'Recovered')
+#     plt.legend()
+#
+#     plt.show()
+#     plt.savefig('all.png')
+#     plt.close()
 
 if __name__ == "__main__":
 
     # countries = api_countries()
     countries = load_countries()
     api_covid(countries = countries)
+    # plot_unique_country(belgium)
